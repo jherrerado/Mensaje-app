297ce2308b5e7c2c6be22aaa104c2893d711c620
jherrerado <jhan.joshua.h.g@gmail.com>
2023-02-09 15:33:17 -0400

    CRUD completion

--- src/main/java/com/jherrera/mensaje_app/MensajeService.java
+++ src/main/java/com/jherrera/mensaje_app/MensajeService.java
@@ -18,14 +18,25 @@ public class MensajeService {
     }
 
     public static void listarMensajes(){
-
+        MensajesDAO.leerMensajeDB();
     }
 
     public static void borrarMensaje(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indica el ID del mensaje a borrar");
+        int id_mensaje =sc.nextInt();
+        MensajesDAO.borrarMensajeDB(id_mensaje);
     }
 
     public static void editarMensaje(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("Indica el ID del mensaje al actualizar");
+        int id_mensaje = sc.nextInt();
+        Mensaje actualizacion = new Mensaje();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
     }
 }
--- src/main/java/com/jherrera/mensaje_app/MensajesDAO.java
+++ src/main/java/com/jherrera/mensaje_app/MensajesDAO.java
@@ -2,6 +2,7 @@ package com.jherrera.mensaje_app;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MensajesDAO {
@@ -25,14 +26,68 @@ public class MensajesDAO {
     }
 
     public static void leerMensajeDB(){
+        Conexion db_connect = new Conexion();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
+
+        try(Connection conexion = db_connect.get_connetion()){
+            String query = "SELECT * FROM mensaje";
+            ps = conexion.prepareStatement(query);
+            rs = ps.executeQuery();
+
+            while(rs.next()){
+                System.out.println("ID: " + rs.getInt("id_mensaje"));
+                System.out.println("Mensaje: " + rs.getString("mensaje"));
+                System.out.println("Autor: " + rs.getString("auto_mensaje"));
+                System.out.println("Fecha: " + rs.getString("fecha_mensaje"));
+                System.out.println(" ");
+            }
+        }catch (SQLException e){
+            System.out.println("no se pudo traer los mensajes");
+            System.out.println(e);
+        }
     }
 
     public static void borrarMensajeDB(int id_mensaje){
+        Conexion db_connect = new Conexion();
 
+        try(Connection conexion = db_connect.get_connetion()){
+            PreparedStatement ps = null;
+                try{
+                    String query = "DELETE FROM mensaje WHERE mensaje.id_mensaje = ?";
+                    ps = conexion.prepareStatement(query);
+                    ps.setInt(1, id_mensaje);
+                    ps.executeUpdate();
+                    System.out.println("El mensaje fue borrado");
+                }catch(SQLException e){
+                    System.out.println(e);
+                    System.out.println("No se pudo borrar el mensaje");
+                }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void actualizarMensajeDB(Mensaje mensaje){
+        Conexion db_connect = new Conexion();
+        try(Connection conexion = db_connect.get_connetion()){
+            PreparedStatement ps = null;
+            try{
+               String query = "UPDATE mensaje SET mensaje = ? WHERE mensaje.id_mensaje = ?";
+               ps = conexion.prepareStatement(query);
+               ps.setString(1, mensaje.getMensaje());
+               ps.setInt(2, mensaje.getId_mensaje());
+               ps.executeUpdate();
+                System.out.println("El mensaje fue actualizado");
 
+            }catch (SQLException e){
+                System.out.println(e);
+                System.out.println("El mensaje no pudo ser actualizado");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+            System.out.println("No se pudo borrar el mensaje");
+        }
     }
 }
